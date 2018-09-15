47862b461635ca44430f2786cb1560b15ed5bad4
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-09-15 03:45:23 -0300

    feat(react): make scripts throw on unhandled rejections

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@
 import get from 'lodash.get'
 import { logMessage } from './utils'
 
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on("unhandledRejection", err => {
+  throw err
+})
+
 const pkg = require(`${process.cwd()}/package.json`)
 
 const babelCoreBridgeVersion = '^7.0.0-bridge.0'
