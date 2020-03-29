7a1ad050a270e54520fc536b05bddd9f670e60c9
Gabriel Duarte <gabrielqduarte@gmail.com>
2020-03-29 01:12:04 +0100

    chore: add release.config to the root

---
+++ release.config.js
@@ -0,0 +1,19 @@
+const baseConfig = require('@gabrielduartem/semantic-release-config')
+
+module.exports = {
+  ...baseConfig,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/npm',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
